b14fe95be04505e5c639a12c217bd8728eb79f30
Lyon <Lyon@Lyon-VAIO>
2014-07-07 11:26:07 +0800

    none

--- dms/src/main/java/com/whr/dms/models/TSuggestion_.java
+++ dms/src/main/java/com/whr/dms/models/TSuggestion_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2014-07-06T21:32:05.589+0800")
+@Generated(value="Dali", date="2014-07-07T11:25:12.257+0800")
 @StaticMetamodel(TSuggestion.class)
 public class TSuggestion_ extends BaseAuditEntity_ {
 	public static volatile SingularAttribute<TSuggestion, String> suggestionTitle;
@@ -13,6 +13,6 @@ public class TSuggestion_ extends BaseAuditEntity_ {
 	public static volatile SingularAttribute<TSuggestion, Date> suggestionDate;
 	public static volatile SingularAttribute<TSuggestion, String> suggestionContent;
 	public static volatile SingularAttribute<TSuggestion, String> author;
-	public static volatile SingularAttribute<TSuggestion, SuggestionState> state;
 	public static volatile SingularAttribute<TSuggestion, SuggestionType> type;
+	public static volatile SingularAttribute<TSuggestion, SuggestionState> state;
 }

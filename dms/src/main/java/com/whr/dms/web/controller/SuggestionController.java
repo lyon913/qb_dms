c758ee4d772c84aaf5914ce1ca25ab348afa6026
Lyon <Lyon@Lyon-PC>
2014-07-21 08:14:39 +0800

    意见簿

--- dms/src/main/java/com/whr/dms/web/controller/SuggestionController.java
+++ dms/src/main/java/com/whr/dms/web/controller/SuggestionController.java
@@ -1,133 +1,117 @@
 package com.whr.dms.web.controller;
 
-import java.util.Date;
-import java.util.List;
-
-import javax.annotation.Resource;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
-import com.whr.dms.models.TReply;
+import com.whr.dms.exceptions.ParameterCheckException;
+import com.whr.dms.models.SuggestionState;
+import com.whr.dms.models.SuggestionType;
 import com.whr.dms.models.TSuggestion;
-import com.whr.dms.models.TUser;
 import com.whr.dms.security.SecurityUtil;
 import com.whr.dms.service.SuggestionReplyService;
 import com.whr.dms.service.SuggestionService;
-import com.whr.dms.service.UserManager;
-import com.whr.dms.web.ajax.ContentRangeHeader;
-import com.whr.dms.web.ajax.DojoSort;
-import com.whr.dms.web.ajax.JsonResponse;
-import com.whr.dms.web.ajax.PageableRange;
 
 @Controller
+@SessionAttributes({"s"})
+@RequestMapping("/suggestion")
 public class SuggestionController {
-	public TUser getUser(){
-		String username = SecurityUtil.getCurrentUserDetials().getUsername();
-		TUser u = um.getTUserByLoginName(username);
-		return u;
-	}
-	
-	@Resource
-	UserManager um;
 	
 	@Autowired
-	SuggestionService sservice;
+	SuggestionService suggServ;
 	@Autowired
 	SuggestionReplyService srservice;
 	
-	@RequestMapping(value="/suggestion/suggestionMana")
-	public String tolist(){
-		return "suggestion/suggestionList";
-	}
-	@RequestMapping(value="/suggestion/suggestionList")
-	public ResponseEntity<List<TSuggestion>> suggestionList(@RequestParam(required = false)String sort,@RequestHeader("Range") String range) {
-		PageableRange pr = new PageableRange(range,new DojoSort(sort).toSpringSort());
-		Page<TSuggestion> page = sservice.getSuggestion(pr);
-		
-		HttpHeaders headers = new HttpHeaders();
-		headers.add("Content-Range", new ContentRangeHeader(pr.getOffset(), page.getNumberOfElements(), page.getTotalElements()).toString());
-		
-		return new ResponseEntity<List<TSuggestion>>(page.getContent(), headers, HttpStatus.OK);
-	}
-	
-
-	@RequestMapping(value = "/suggestion/create")
-	public String noticeCreatePage(){
+	/**
+	 * 初始化创建意见表单
+	 * @param m
+	 * @return
+	 */
+	@RequestMapping(value = "/new", method = RequestMethod.GET)
+	public String initCreateForm(Model m) {
+		TSuggestion s = new TSuggestion();
+		s.setType(SuggestionType.Suggestion);
+		s.setState(SuggestionState.Private);
+		m.addAttribute("s", s);
 		return "suggestion/createSuggestion";
-	}
-	
-
-	
-	
-	@RequestMapping(value="/suggestion",method = RequestMethod.POST)
-	public @ResponseBody JsonResponse saveSuggestion(TSuggestion suggestion){
-		try{
-			TUser user = getUser();
-			suggestion.setAuthorId(user.getId());
-			suggestion.setSuggestionDate(new Date());
-			suggestion.setAuthor(user.getName());
-			sservice.saveSuggestion(suggestion);
-			return new JsonResponse(true, null, suggestion.getId());
-		} catch(Exception e){
-			return new JsonResponse(false, e.getMessage(), null);
-		}
-	}
-	
-	@RequestMapping(value="/suggestion/edit/{id}")
-	public String prepareEdit(@PathVariable long id,Model model){
-		TSuggestion s = sservice.getById(id);
-		if(s.getAuthorId() != getUser().getId()){
-			throw new AccessDeniedException("只有作者才能修改该意见。");
-		}
-		model.addAttribute("suggestion", s);
-		return "suggestion/createSuggestion";
-	}
-	
-	@RequestMapping(value="/suggestion/{id}")
-	public String prepareRead(@PathVariable long id,Model model){
-		TSuggestion s = sservice.getById(id);
-		List<TReply> list = srservice.getSuggestionReply(id);
-		model.addAttribute("suggestion", s);
-		model.addAttribute("replyList",list);
-		return "suggestion/readSuggestion";
 	}
 	
 	/**
-	 * 在全部意见中查找
-	 * @param sort
-	 * @param range
+	 * 处理创建意见表单
+	 * @param s
+	 * @param bind
+	 * @param status
 	 * @return
 	 */
-	@RequestMapping(value = "/suggestion/searchAll", method = RequestMethod.POST)
-	public @ResponseBody List<TSuggestion> searchAll(String key) {
-		return sservice.searchAllSuggestions(key);
-
+	@RequestMapping(value = "/new", method = RequestMethod.POST)
+	public String processCreateForm(@ModelAttribute @Valid TSuggestion s,BindingResult bind,SessionStatus status) {
+		if(bind.hasErrors()) {
+			return "suggestion/createSuggestion"; 
+		}
+		
+		suggServ.saveSuggestion(s);
+		status.setComplete();
+		return "suggestion/list";
+	}
+	/**
+	 * 初始化修改意见表单
+	 * @param id
+	 * @param m
+	 * @return
+	 * @throws ParameterCheckException
+	 */
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public String initUpdateForm(@PathVariable long id,Model m) throws ParameterCheckException {
+		TSuggestion s = suggServ.getById(id);
+		if(s == null) {
+			throw new ParameterCheckException("未找到此记录");
+		}
+		
+		if(!SecurityUtil.isMe(s.getAuthorId())) {
+			throw new AccessDeniedException("只有作者本人才能修改");
+		}
+		
+		if(SuggestionState.Public.equals(s.getState())) {
+			throw new AccessDeniedException("意见已经过审核，不能修改。");
+		}
+		m.addAttribute("s", s);
+		return "suggestion/createSuggestion";
+	}
+	
+	/**
+	 * 处理意见簿修改表单
+	 * @param s
+	 * @param bind
+	 * @param status
+	 * @return
+	 */
+	public String processUpdateForm(TSuggestion s,BindingResult bind,SessionStatus status) {
+		if(bind.hasErrors()) {
+			return "suggestion/createSuggestion";
+		}
+		
+		suggServ.saveSuggestion(s);
+		status.setComplete();
+		return "suggestion/createSuggestion";
 	}
 	
 	/**
 	 * 
 	 * @return
 	 */
-	@RequestMapping("/suggestion/del/{id}")
-	public @ResponseBody JsonResponse deleteSuggestion(@PathVariable long id){
-		try{
-			sservice.deleteSuggestion(id);
-			return new JsonResponse(true, null, null);
-		}catch(Exception e){
-			return new JsonResponse(false, e.getMessage(), null);
-		}
+	@RequestMapping("/assess/list")
+	public String privateList() {
+		
 	}
+
 }

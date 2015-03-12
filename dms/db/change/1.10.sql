d9a29aad0a1d0c8e0751527be95d454d3aac967b
Lyon Chang <lyon_z@msn.com>
2015-03-12 11:19:13 +0800

    tsuggestion表增加索引voidId

--- dms/db/change/1.10.sql
+++ dms/db/change/1.10.sql
@@ -3,5 +3,8 @@ ADD INDEX `IDX_TVOTERECORD_VOTEID` (`voteId` ASC),
 ADD INDEX `IDX_TVOTERECORD_USERID` (`userId` ASC),
 ADD INDEX `IDX_TVOTERECORD_OPTIONID` (`optionId` ASC);
 
+ALTER TABLE `dms`.`tsuggestion` 
+add index `voteId` (`voteId` ASC);
+
 insert into TDatabaseChange(majorVersion,minorVersion,fixVersion,fileName,opdate) 
 	values(1,10,0,'1.10.sql',now());
